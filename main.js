8ad22b46d2cd1de216983d8e0508f19efe0502a9
Mirko <mirkoanna137@gmail.com>
2022-01-19 16:18:04 +0100

    Chiedi agli utenti informazioni

---
+++ main.js
@@ -0,0 +1,11 @@
+//Chiedi all'utente il suo nome
+let nomeUtente = prompt('Inserisci nome');
+console.log(nomeUtente)
+
+//Chiedi all'utente il suo cognome
+let cognomeUtente = prompt('Inserisci cognome')
+console.log(cognomeUtente)
+
+//Chiedi all'utente il suo colore preferito
+let coloreUtente = prompt('Inserisci colore preferito')
+console.log(coloreUtente)
