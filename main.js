0fb04558ffca1f601c62a67e47264e3b0396979c
Mirko <mirkoanna137@gmail.com>
2022-01-19 16:30:29 +0100

    Genera password (Fine esercizio)

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ console.log(cognomeUtente)
 //Chiedi all'utente il suo colore preferito
 let coloreUtente = prompt('Inserisci colore preferito')
 console.log(coloreUtente)
+
+
+
+//PASSWORD
+
+//Generatore password console
+const password = nomeUtente + cognomeUtente + coloreUtente;
+console.log(password + "21")
+
+//Generatore password html
+document.getElementById('password').innerHTML = "La tua password è " + nomeUtente + cognomeUtente + coloreUtente + "21";
